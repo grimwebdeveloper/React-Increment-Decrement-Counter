41742ea85bb8c46672e36d82524003fe2ac051ac
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2024-08-01 19:23:48 +0530

    Initial commit updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="app">
+      <h1 className="heading">Increment & Decrement Operator</h1>
+      <div className="count">0</div>
+      <div className="buttons">
+        <button type="button" className="btn decrement">Decrement</button>
+        <button type="button" className="btn reset">Reset</button>
+        <button type="button" className="btn increment">Increment</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
